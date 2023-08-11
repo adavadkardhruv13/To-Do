da8e27124ef7853e1b8f367d4cf8dfab565c0365
Dhruv Adavadkar <adavadkardhruv@gmail.com>
2023-08-12 00:53:29 +0530

    Added google auth

--- to_do/settings.py
+++ to_do/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -151,4 +151,3 @@ AUTHENTICATION_BACKENDS = {
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-django_heroku.settings(locals())
